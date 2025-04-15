99d8313445fe55321d04424b84d9e86c0117e0b7
nivedha-bhaskaran <nivedhabhaskaran1510@gmail.com>
2025-04-15 16:47:55 +0530

    Create CostumeUpload.js

---
+++ CostumeUpload.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FirstPage.css'
+const FirstPage = () => {
+  return (
+    <div>
+        <img className="openbg" alt="not found" src="https://t4.ftcdn.net/jpg/05/96/62/65/360_F_596626503_jrzjZNYStDexiWxQFqO7oCh6M8PdMlJs.jpg"></img>
+        <h1>Check Your Costume</h1>
+    </div>
+  )
+}
+
+export default FirstPage
